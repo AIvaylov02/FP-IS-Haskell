c5dd633bc3f4299d00b753b9a32ad7efd3953f23
Antoan Ivaylov <tonimen_31@abv.bg>
2025-03-05 10:26:31 +0200

    Fixed my solution for 22-11.hs in week 1\nIn the future the same should be done with 22-9.hs, also maybe add explicit declarations for the functions

--- week1/22-11.hs
+++ week1/22-11.hs
@@ -2,12 +2,15 @@
 -- Например, 6 е съвършено, защото 6 = 1+2+3; числото 1 не е съвършено. 
 -- Да се дефинира функция, която проверява дали дадено положително цяло число е съвършено.
 
--- TODO refactor
-isPerfect :: Int -> Bool
-isPerfect n = n == sum (getDivisors n)
-    where getDivisors divisor xs
-            | getDivisors n 1 xs = 1:xs
-            | getDivisors n divisor xs = if n `mod` divisor == 0
-                                         then divisor:(getDivisors n (divisor - 1) xs)
-                                         else getDivisors n (divisor - 1) xs    
+getDivisors :: (Integral a) => a -> [a]
+getDivisors n = getDivisorsHelper n n []
+    where
+    getDivisorsHelper :: (Integral a, Eq a) => a -> a -> [a] -> [a]
+    getDivisorsHelper n 1 xs = 1:xs
+    getDivisorsHelper n divisor xs = if n `mod` divisor == 0 
+                            then divisor:(getDivisorsHelper n (divisor - 1) xs)
+                            else getDivisorsHelper n (divisor - 1) xs
+
+isPerfect :: (Integral a) => a -> Bool
+isPerfect n = sum (tail (getDivisors n)) == n
            
